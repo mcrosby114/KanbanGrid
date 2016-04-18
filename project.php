3c72df514ebf3878f19c9a528640d93a38a47161
mcrosby114 <mcrosby114@gmail.com>
2016-04-18 04:44:38 -0600

    Adjusting CSS for grid page table view

---
+++ project.php
@@ -0,0 +1,9 @@
+<?php
+class Project {
+  private $title;
+  private $row;
+  private $due_date;
+  private $color;
+  private $descrip;
+}
+?>
---
+++ task.php
@@ -0,0 +1,9 @@
+<?php
+class Task {
+  private $title;
+  private $col;
+  private $due_date;
+  private $color;
+  private $descrip;
+}
+?>
