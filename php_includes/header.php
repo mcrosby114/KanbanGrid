a9eeeb373953a15c6c4d155ba9d2d6bf9d6e48d5
mcrosby114 <mcrosby114@gmail.com>
2016-04-17 22:05:41 -0600

    moved dao

--- php_includes/header.php
+++ php_includes/header.php
@@ -1,5 +1,7 @@
-<?php if(!isset($_SESSION)) session_start();
-      require_once ("dao.php");
+<?php if(!isset($_SESSION)) {
+  session_start();
+  }
+  require_once ("dao.php");
 ?>
 
 <?php if(isset($_SESSION["access_granted"]) && $_SESSION["access_granted"]) {
