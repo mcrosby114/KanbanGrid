d70533c6ea9055fdb18509859c503b52432db01b
mcrosby114 <mcrosby114@gmail.com>
2016-04-25 18:28:37 -0600

    Added real text

--- index.php
+++ index.php
@@ -46,23 +46,38 @@
       </form>
 
       <div class="bodysection" id="sec1">
-        <h2>SECTION 1 Intro Text, Img, Signup Form</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac mauris sed libero rutrum congue at eget elit. Maecenas a ex eu leo porttitor iaculis. Curabitur egestas eu massa sed condimentum. Nulla facilisi. Proin tortor justo, pretium id rutrum sed, lobortis in nunc. Cras pulvinar sodales massa quis dapibus. Nulla non dolor sit amet ligula aliquet elementum.</p>
+        <h2>Let KanbanGrid Map your Workflow</h2>
+        <h4>KanbanGrid is a simple, intuitive, and flexible way to map out your weekly projects and tasks.</h4>
         <img id-"testimg" src="images/business-idea-534228_cropped.jpg" height=400px width=1000px alt="Kanban Example" title="KanbanGrid in Action"/>
-        <p>Maecenas at laoreet metus. Donec lobortis tempor dolor vitae rutrum. Sed aliquam porta lacus vitae volutpat. Pellentesque suscipit urna at augue aliquet, eu condimentum enim hendrerit. Maecenas volutpat, nibh sed semper dignissim, ipsum massa elementum lacus, nec tincidunt erat quam luctus turpis. Mauris pellentesque ipsum eu mauris suscipit tempus. Donec ut libero aliquet, maximus eros vitae, blandit ligula. Curabitur purus turpis, mattis vitae eleifend eu, ultricies vel nunc. Donec nec justo purus. Duis tempor purus ac risus aliquam pulvinar.</p>
+        <p>Kanban helps you harness the power of visual information by using sticky notes on a whiteboard to create a “picture” of your work. Seeing how your work flows within your team’s process lets you not only communicate status but also give and receive context for the work. Kanban takes information that typically would be communicated via words and turns it into brain candy.</p>
+        <p>
+          Unlike other methods that force fit change from the get-go, Kanban is about evolution, not revolution. It hinges on the fundamental truth that you can’t get where you want to go without first knowing where you are.
+        </p>
+        <p>
+          Kanban is gaining traction as a way to smoothly implement Agile and Lean management methods in tech and non-tech companies around the world. Throughout this fresh take on Toyota’s manufacturing process, Kanban’s core elements have remained rooted in the principles below.
+        </p>
+        <p>
+          By creating a visual model of your work and workflow, you can observe the flow of work moving through your Kanban system. Making the work visible—along with blockers, bottlenecks and queues—instantly leads to increased communication and collaboration.
+        </p>
       </div>
 
       <div class="bodysection" id="sec2img">
-        <h2>SECTION 2: Wide Image</h2>
+        <h2>Get Your Weekly Agenda Under Control</h2>
         <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/Kanban_board.png" alt="Kanban Example" title="KanbanGrid in Action"/>
       </div>
 
       <div class="bodysection" id="sec3">
-        <h2>SECTION 3: User reviews (Text in columns or boxes)</h2>
-        <p>Donec egestas tortor ac mi vehicula consequat. Donec eleifend porttitor consectetur. Sed sed commodo purus, vitae viverra dui. Suspendisse potenti. Nullam quis tellus tempus, tincidunt elit sed, posuere leo. Pellentesque non sapien feugiat, iaculis libero a, lacinia quam. Pellentesque eu sem bibendum, efficitur erat id, aliquet odio. Mauris sit amet dictum velit. Aliquam sagittis tincidunt arcu, sit amet pharetra lorem pretium nec. In efficitur tristique ultricies.</p>
+        <h2>Flexibility in Planning</h2>
+        <p>By limiting how much unfinished work is in process, you can reduce the time it takes an item to travel through the Kanban system. You can also avoid problems caused by task switching and reduce the need to constantly reprioritize items.</p>
+        <p>
+          By using work-in-process (WIP) limits and developing team-driven policies, you can optimize your Kanban system to improve the smooth flow of work, collect metrics to analyze flow, and even get leading indicators of future problems by analyzing the flow of work.
+        </p>
+        <p>
+          Once your Kanban system is in place, it becomes the cornerstone for a culture of continuous improvement. Teams measure their effectiveness by tracking flow, quality, throughput, lead times and more. Experiments and analysis can change the system to improve the team’s effectiveness.
+        </p>
         <img src="images/kanban7_cropped.jpg" height=400px width=1000px alt="Kanban Example" title="KanbanGrid in Action"/>
-        <p>Fusce sed lorem venenatis, efficitur turpis sit amet, elementum dolor. Nam sed pharetra urna. Aenean laoreet eu ex ac tristique. Phasellus consectetur rutrum condimentum. Sed ut pharetra nulla. Etiam blandit ac augue nec tempor. Nunc in ante nec leo tincidunt eleifend. Nam quis dolor malesuada tellus pellentesque sodales ut et sapien.</p>
-        <p>Vivamus ornare cursus lorem sit amet faucibus. Sed tincidunt libero metus. Proin elit eros, iaculis non sollicitudin consequat, iaculis sit amet lorem. Maecenas eget egestas sem, sit amet ultrices nisi. Curabitur pellentesque felis quis elit iaculis maximus. Mauris dignissim malesuada bibendum. Nulla et eros enim. Morbi et nulla id erat consectetur luctus ut non dolor. Nulla ut finibus nibh, nec interdum augue. Nam pretium rutrum erat, id lobortis magna. Curabitur iaculis lectus vitae diam sodales, nec hendrerit tellus fringilla. Donec ut leo eget nisl accumsan suscipit.</p>
+        <p>Kanban became an effective tool to support running a production system as a whole, and an excellent way to promote improvement. Problem areas are highlighted by reducing the number of kanban in circulation.</p>
+        <p>One of the main benefits of kanban is to establish an upper limit to the work in progress inventory, avoiding overloading of the manufacturing system. Although producing software is a creative activity and therefore different to mass-producing cars, the underlying mechanism for managing the production line can still be applied.</p>
       </div>
     </body>
 
