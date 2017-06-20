b47683c834f1e93953ce96f126444282b0e9baba
Matthew Crosby <mcrosby114@gmail.com>
2017-06-20 06:58:37 -0600

    removed all but the README.md file from this branch

--- project.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Project {
-  private $title;
-  private $row;
-  private $due_date;
-  private $color;
-  private $descrip;
-}
-?>
--- task.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Task {
-  private $title;
-  private $col;
-  private $due_date;
-  private $color;
-  private $descrip;
-}
-?>
